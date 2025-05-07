ffd0ca649659de477d5af2bf1a87ececba98fa4c
Schneider <schneidercorrea@gmail.com>
2025-05-07 11:44:14 -0500

    creacion de modulo database y entidad Track

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //TODO: descomentar en producción
+  //app.setGlobalPrefix("api/v1");
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  //TODO: enviar puerto por variable de entorno
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
---
+++ src/track/entities/track.entity.ts
@@ -0,0 +1,97 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum TrackStatus {
+  UNPUBLISHED = 'unpublished',
+  ON_RECORDING = 'on_recording',
+  RECORDED = 'recorded',
+}
+
+@Entity('tracks')
+export class Track {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  //TODO: crear la relación
+  genre: string;
+
+  @Column('varchar', { name: 'sub_genre' })
+  subGenre: string;
+
+  //TODO: por default url del logo de la app
+  @Column({ type: 'varchar', nullable: true, default: 'urllogoapp.img' })
+  cover: string;
+
+  /*TODO: confirmar si viene del servicio de alojamiento
+  @Column('int', { name: 'duration_ms' })
+  durationMs: number;
+  */
+
+  @Column('varchar')
+  url: string;
+
+  @Column('int')
+  year: number;
+
+  @Column('varchar')
+  language: string;
+
+  @Column('varchar')
+  lyric: string;
+
+  @Column('simple-json', { name: 'externals_ids', nullable: true })
+  externalsIds: Record<string, string>;
+
+  @Column('varchar', { name: 'split_sheet', nullable: true })
+  splitSheet: string;
+
+  @Column({
+    name: 'track_status',
+    type: 'enum',
+    enum: TrackStatus,
+    default: TrackStatus.UNPUBLISHED,
+  })
+  trackStatus: TrackStatus;
+
+  @Column('simple-json', { name: 'certificates_DNDA', nullable: true })
+  certificatesDNDA: Record<string, string>;
+
+  @Column('boolean', { default: true })
+  available: boolean;
+
+  //TODO: crear relacion
+  author: string;
+
+  @Column('simple-json')
+  awards: Record<string, string>;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  deletedAt: Date;
+}
